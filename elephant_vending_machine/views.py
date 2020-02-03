bb792d620e7aa7cb40b800bc9b91cf1f96653fe8
Nicholas F Male <nmale98@gmail.com>
2020-02-02 22:45:36 -0500

    Adding routes and dummy responses for iteration 1 along with unit tests. (#4)
    Adding dummy routes for log retrieval, image upload, trial running.

--- elephant_vending_machine/views.py
+++ elephant_vending_machine/views.py
@@ -6,6 +6,7 @@ a lot of routes.
 """
 # Circular import OK here. See https://flask.palletsprojects.com/en/1.1.x/patterns/packages/
 # pylint: disable=cyclic-import
+from flask import request
 from elephant_vending_machine import APP
 
 @APP.route('/')
@@ -20,3 +21,59 @@ def index():
         HTTP response OK with payload 'Hello Elephants'
     """
     return 'Hello Elephants!'
+
+@APP.route('/run-trial', methods=['POST'])
+def run_trial():
+    """Responds with 'Running {trial_name}' string
+
+    All requests sent to this route should have a trial_name in
+    the query string, otherwise a 400 error will be returned
+
+    Returns:
+        HTTP response 200 with payload 'Running {trial_name}' or
+        HTTP response 400 with payload 'No trial_name specified'
+    """
+    response = ""
+    if request.args.get('trial_name') is not None:
+        response = 'Running ' + request.args.get('trial_name')
+    else:
+        response = 'No trial_name specified'
+    return response
+
+
+@APP.route('/add-image', methods=['POST'])
+def upload_image():
+    """Responds with 'File received' string
+
+    All requests sent to this route should have an image file
+    included in the body of the request, otherwise a 400 error
+    will be returned
+
+    Returns:
+        HTTP response 200 with payload 'File received' or
+        HTTP response 500 with payload 'No image file in request.'
+    """
+    response = ""
+    if 'file' in request.files:
+        response = "File received."
+    else:
+        response = "No image file in request."
+    return response
+
+@APP.route('/log', methods=['GET'])
+def log():
+    """Returns the specified log file
+
+    All requests sent to this route should have a log_name in
+    the query string, otherwise a 400 error will be returned
+
+    Returns:
+        HTTP response 200 if log file exists, or HTTP response 500
+        if it does not.
+    """
+    response = ""
+    if request.args.get('log_name') is not None:
+        response = 'This would be ' + request.args.get('log_name')
+    else:
+        response = 'Error with request.'
+    return response
--- tests/test_views.py
+++ tests/test_views.py
@@ -13,3 +13,23 @@ def client():
 def test_root_route(client):
     response = client.get('/')
     assert b'Hello Elephants!' in response.data
+
+def test_run_trial_route_success(client):
+    response = client.post('/run-trial?trial_name=demo')
+    assert b'Running demo' in response.data
+
+def test_run_trial_route_empty_query_string(client):
+    response = client.post('/run-trial')
+    assert b'No trial_name specified' in response.data
+
+def test_add_image_route_no_file(client):
+    response = client.post('/add-image')
+    assert b'No image file in request' in response.data
+
+def test_get_log_route_success(client):
+    response = client.get('/log?log_name=alog')
+    assert b'This would be alog' in response.data
+
+def test_get_log_route_no_log_name(client):
+    response = client.get('/log')
+    assert b'Error with request.' in response.data
