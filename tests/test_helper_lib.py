8e0d95fcfdee196e547bf28b47cc3213b00be3c5
riyalengade <riya.lengade@gmail.com>
2020-03-02 16:45:14 -0500

    more tests for helper_library

--- tests/test_helper_lib.py
+++ tests/test_helper_lib.py
@@ -3,6 +3,41 @@ import pytest
 from elephant_vending_machine import elephant_vending_machine
 from elephant_vending_machine import helper_library
 from elephant_vending_machine.helper_library import helper_library
+from elephant_vending_machine.screens import Screens
+
+@pytest.fixture
+def screen1():
+    screen_1 = Screens()
+    screen_1.set_screen(1)
+    screen_1.set_stimuli('/images/test_image.jpeg')
+    screen_1.set_stimuli_flag(False)
+    return screen_1
+
+@pytest.fixture
+def screen2(): 
+    screen_2 = Screens()
+    screen_2.set_screen(2)
+    screen_2.set_stimuli('/images/test_image2.jpeg')
+    screen_2.set_stimuli_flag(False)
+    return screen_2
+
+@pytest.fixture
+def screen3(): 
+    screen_3 = Screens()
+    screen_3.set_screen(3)
+    screen_3.set_stimuli('/images/test_image3.jpeg')
+    screen_3.set_stimuli_flag(True)
+    return screen_3
+
+def test_display_stimuli_one(screen1):
+    helper_library.display_stimuli(1, '/images/test_image.jpeg', False)
+    assert helper_library.L_screen.get_s
+
+def test_display_stimuli_two(screen2): 
+    helper_library.display_stimuli(2, '/images/test_image2.jpeg', False)
+
+def test_display_stimuli_three(screen3): 
+    helper_library.display_stimuli(3, '/images/test_image3.jpeg', True)
 
 def test_inter_trial_time():
     time = 0
